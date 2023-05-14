735bc04ab9eb94ba89bd7db1350c4837e65b3aad
Kyle <kyle.park@intel.com>
2023-05-14 15:20:10 +0900

    build_libtorch

--- scripts/build_libtorch.sh
+++ scripts/build_libtorch.sh
@@ -2,15 +2,24 @@
 set -e
 
 # option format: space<option>) # <comments>
-usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+usage() { echo "$0 usage:" && grep " .)\ #" $0 && echo "Example: $0 -v 2.0.1 -w /home/intel/work/pkg_build/libtorch-2.0"; exit 0; }
 
 MKL=1
+TBB=0
 WORK_DIR=`pwd`
 PYTHON_VER=3.9
-while getopts "htw:" opt; do
+# libtorch version
+PYTORCH_VER="main"
+while getopts "hatv:w:" opt; do
     case ${opt} in
-        t) # Try to use ATen library without MKL; default is to build with MKL enabled. Should build where MKL cannot be found by CMake: ex) libtorch_nomkl venv
+        a) # Try to use ATen library without MKL; default is to build with MKL enabled. Should build where MKL cannot be found by CMake: ex) libtorch_nomkl venv
             MKL=0
+            ;;
+        t) # Use TBB instead of OpenMP.
+            TBB=1
+            ;;
+        v) # PyTorch version to specify. Default is main
+            PYTORCH_VER=$OPTARG
             ;;
         w) # working directory where pytorch packages are downloaded and built; default is current working directory where this script is called
             WORK_DIR=$OPTARG
@@ -55,6 +64,7 @@ init() {
     # Should stop here if required venv is not found
     source $anaconda_dir/etc/profile.d/conda.sh 
     if { conda env list | grep "$conda_env"; } >/dev/null 2>&1; then
+        log "Activated venv $conda_env"
         conda activate $conda_env
     else
         log "Creating a venv $conda_env..."
@@ -62,50 +72,60 @@ init() {
     fi
 }
 
-build_cpu_libtorch() {
+# MKL enabled libtorch
+build_mkl_libtorch() {
     pushd .
     cd $WORK_DIR
-    # internal version
-    pytorch_git=https://github.com/intel-innersource/frameworks.ai.pytorch.private-gpu
-    # pytorch_dir=frameworks.ai.pytorch.private-gpu
+    # public version
+    pytorch_git=https://github.com/pytorch/pytorch.git
+    # You can modify as necesseary
     pytorch_dir=pytorch
+    # internal version
+    # pytorch_git=https://github.com/intel-innersource/frameworks.ai.pytorch.private-gpu
+    # pytorch_dir=frameworks.ai.pytorch.private-gpu
     if [ ! -d "$pytorch_dir" ]; then
-        log "Cloning from $pytorch_git"
-        git clone $pytorch_git
+        log "Cloning from $pytorch_git to $WORK_DIR/$pytorch_dir"
+        git clone $pytorch_git $pytorch_dir
     fi
     cd $pytorch_dir
+    git checkout ${PYTORCH_VER}
     git submodule sync && git submodule update --init --recursive
     pip install -r requirements.txt
 
     log "Building PyTorch"
 
-    libtorch_dir=libtorch-2.1.0-nomkl
-    rmdir ../${libtorch_dir}-build
+    if [ $TBB == 0 ]; then
+        libtorch_dir=libtorch-${PYTORCH_VER}-mkl
+    else
+        libtorch_dir=libtorch-${PYTORCH_VER}-mkl-tbb
+    fi
+
+    rm -rf ../${libtorch_dir}-build
     mkdir ../${libtorch_dir}-build
     cd ../${libtorch_dir}-build
     export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-    pip install -r ../pytorch/requirements.txt
 
-    # conda uninstall mkl
-    # sudo mv /opt/intel/oneapi/mkl /opt/intel/oneapi/mkl_tmp
-    # CMake Warning:
-    #  Manually-specified variables were not used by the project:
-    #  USE_MKL
-    cmake -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=../${libtorch_dir} \
-    -DUSE_CUDA:BOOL=OFF -DUSE_MPI:BOOL=OFF -DUSE_KINETO:BOOL=OFF -DBUILD_BINARY:BOOL=OFF \
-    -DUSE_MKL:BOOL=OFF -DUSE_BLAS=0 -DUSE_MKLDNN=ON -DUSE_OPENMP=ON -DUSE_TBB=OFF \
-    ../${pytorch_dir} 2>&1 | tee -a ./cmake.log
-    # sudo mv /opt/intel/oneapi/mkl_tmp /opt/intel/oneapi/mkl
+    if [ $TBB == 0 ]; then
+        cmake -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=../${libtorch_dir} \
+        -DUSE_CUDA:BOOL=OFF -DUSE_MPI:BOOL=OFF -DUSE_KINETO:BOOL=OFF -DBUILD_BINARY:BOOL=OFF \
+        -DUSE_MKL:BOOL=OFF -DUSE_BLAS=0 -DUSE_MKLDNN=ON -DUSE_OPENMP=ON -DUSE_TBB=OFF \
+        ../${pytorch_dir} 2>&1 | tee -a ./cmake.log
+    else
+        cmake -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=../${libtorch_dir} \
+        -DUSE_CUDA:BOOL=OFF -DUSE_MPI:BOOL=OFF -DUSE_KINETO:BOOL=OFF -DBUILD_BINARY:BOOL=OFF \
+        -DUSE_TBB:BOOL=ON -DUSE_SYSTEM_TBB:BOOL=ON -DCMAKE_PREFIX_PATH=/opt/intel/oneapi/tbb/latest \
+        -DATEN_THREADING=TBB -DMKL_THREADING=TBB -DBLAS=MKL -DMKLDNN_CPU_RUNTIME=TBB -DUSE_MKLDNN=1 \
+        ../${pytorch_dir} 2>&1 | tee -a ./cmake.log
+    fi
     
     cmake --build . --target install --config Release -- -j 2>&1 | tee ./build.log
-
     unset CMAKE_PREFIX_PATH
 
     # build-hash, build-version
     cd ../${libtorch_dir}
     echo "$(pushd ../${pytorch_dir} && git rev-parse HEAD)" > ./build-hash
     # ../${pytorch_dir}/torch/csrc/api/include/torch/version.h 
-    echo "2.1.0" > ./build-version
+    echo "${PYTORCH_VER}" | sed -r 's/v//g' > ./build-version
 
     popd
 }
@@ -136,15 +156,62 @@ build_cpu_ipexlibtorch() {
     popd
 }
 
+build_aten_libtorch() {
+    pushd .
+    cd $WORK_DIR
+    # internal version
+    pytorch_git=https://github.com/intel-innersource/frameworks.ai.pytorch.private-gpu
+    # pytorch_dir=frameworks.ai.pytorch.private-gpu
+    pytorch_dir=pytorch
+    if [ ! -d "$pytorch_dir" ]; then
+        log "Cloning from $pytorch_git"
+        git clone $pytorch_git $pytorch_dir
+    fi
+    cd $pytorch_dir
+    git submodule sync && git submodule update --init --recursive
+    pip install -r requirements.txt
+
+    log "Building PyTorch"
+
+    libtorch_dir=libtorch-2.1.0-nomkl
+    rm -rf ../${libtorch_dir}-build
+    mkdir ../${libtorch_dir}-build
+    cd ../${libtorch_dir}-build
+    export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+
+    # conda uninstall mkl
+    # sudo mv /opt/intel/oneapi/mkl /opt/intel/oneapi/mkl_tmp
+    # CMake Warning:
+    #  Manually-specified variables were not used by the project:
+    #  USE_MKL
+    cmake -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=../${libtorch_dir} \
+    -DUSE_CUDA:BOOL=OFF -DUSE_MPI:BOOL=OFF -DUSE_KINETO:BOOL=OFF -DBUILD_BINARY:BOOL=OFF \
+    -DUSE_MKL:BOOL=OFF -DUSE_BLAS=0 -DUSE_MKLDNN=ON -DUSE_OPENMP=ON -DUSE_TBB=OFF \
+    ../${pytorch_dir} 2>&1 | tee -a ./cmake.log
+    # sudo mv /opt/intel/oneapi/mkl_tmp /opt/intel/oneapi/mkl
+    
+    cmake --build . --target install --config Release -- -j 2>&1 | tee ./build.log
+
+    unset CMAKE_PREFIX_PATH
+
+    # build-hash, build-version
+    cd ../${libtorch_dir}
+    echo "$(pushd ../${pytorch_dir} && git rev-parse HEAD)" > ./build-hash
+    # ../${pytorch_dir}/torch/csrc/api/include/torch/version.h 
+    echo "2.1.0" > ./build-version
+
+    popd
+}
+
 init
 
 if [ $MKL == 1 ]; then
     log "Building MKL enabled libtorch"
 
-    build_cpu_libtorch
+    build_mkl_libtorch
 
 else
-    log "Building $DEVICE PyTorch"
+    log "Building ATen based PyTorch"
 
     build_aten_pytorch
 fi
